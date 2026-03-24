15ce48a8fd080736d0ded9081ee0f7155d536572
Seshanand <seshanand04@gmail.com>
2026-03-24 13:42:05 +0530

    changed oneline

--- show_info.py
+++ show_info.py
@@ -3,6 +3,6 @@ from datetime import date, datetime
 
 
 print("Code Push Detected!")
-today = datetime.date.today()
+today = datetime.today()
 print(f"Date: {today}")
 print(f"Time: {datetime.datetime.now().strftime('%H:%M:%S')}")

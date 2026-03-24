edd85f03417201b2d0369225a1636216156e60a1
Seshanand <seshanand04@gmail.com>
2026-03-24 12:10:32 +0530

    cleared 2 lines

--- show_info.py
+++ show_info.py
@@ -1,7 +1,6 @@
 import datetime import date, datetime
 
-print("==========================")
+
 print("Code Push Detected!")
 print(f"Date: {datetime.date.today()}")
 print(f"Time: {datetime.datetime.now().strftime('%H:%M:%S')}")
-print("==========================")
